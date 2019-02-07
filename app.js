015a11a1cce31cf494044994715d31acba15b6cd
Sarah Barnes <sarah@sarahbarnesweb.com>
2019-02-07 00:41:27 -0500

    working on checkwin

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const letters = document.getElementsByClassName('letter');
 const buttons = document.getElementsByTagName('button');
 const scoreboard = document.getElementById('scoreboard');
 
-
+var score;
 var missed = 0;
 
 const phrases = [
@@ -19,7 +19,7 @@ const phrases = [
 
 //Hides the Wheel of Success overlay to display game
 overlay.addEventListener('click', (e) => {
-    if (e.target.className === 'btn__reset') {
+    if (e.target.textContent === 'Start Game') {
         overlay.style.display = 'none';
     }
 });
@@ -60,23 +60,36 @@ function checkLetter(letterkeys) {
 	return check;
 }
 
-function checkwin(){
 
-}
+//Each time the player guesses a letter, this function will check whether the game has been won or lost. At the very end of the keyboard event listener, you’ll run this function to check if the number of letters with class “show” is equal to the number of letters with class “letters”. If they’re equal, show the overlay screen with the “win” class and appropriate text. Otherwise, if the number of misses is equal to or greater than 5, show the overlay screen with the “lose” class and appropriate text.
+
+function checkwin(){
+		let x = document.querySelectorAll('#phrase show').length;
+		let y = document.querySelectorAll('#qwerty letters').length;
+
+	  if (missed <= 5 && x.length === y.length) {
+	    overlay.style.display = 'win';
+	  } else {
+			overlay.style.display = 'lose';
+		}
+
+}//TODO: Fix checkwin();
 
 qwerty.addEventListener('click', (e) => {
 	if (e.target.tagName === 'BUTTON'){
-		let letterFound = checkLetter(e.target);
+	  let letterFound = checkLetter(e.target);
+
 	 	e.target.classList.add('chosen');
 		e.target.disabled = true;
 		if (letterFound !== true) {
 			missed += 1;
-
-	  	let ol = scoreboard.getElementsByTagName('ol')[0];
-			let li = scoreboard.querySelector('li:last-child');
-	  	ol.removeChild(li);
+			if (missed <= 5) {
+		  	let ol = scoreboard.getElementsByTagName('ol')[0];
+				let li = scoreboard.querySelector('li:last-child');
+		  	ol.removeChild(li);
+			}
 		}
+		checkwin();
 	}
 
-	console.log(missed);
 });
