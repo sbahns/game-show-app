469d3be4f0af1c442daa06937e12b53c3c27430e
Sarah Barnes <sarah@sarahbarnesweb.com>
2019-02-09 16:35:29 -0500

    fixed game reset

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const overlay = document.getElementById('overlay');
 const reset = document.querySelector('.btn__reset');
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const ul = document.querySelector('ul');
+const ul = document.getElementsByTagName('ul')[0];
 const letters = document.getElementsByClassName('letter');
 const show = document.getElementsByClassName('show');
 const buttons = document.getElementsByTagName('button');
@@ -14,12 +14,11 @@ var resetGame = false;
 
 const phrases = [
 	'We are the Champions',
-	'Wassup',
+	'Crazy little thing called love',
 	'Another one bites the dust',
 	'Is this the real life',
-	'Elementary my dear Watson'
+	'All we hear is radio gaga'
 ]
-
 
 
 //Hides the Wheel of Success overlay to display game
@@ -27,6 +26,9 @@ function clearOverlay(display, cssClass){
 	overlay.addEventListener('click', (e) => {
 		overlay.style.display = display;
 		overlay.classList.add(cssClass);
+		if (resetGame === true) {
+			gamereset();
+		}
 	})
 }
 
@@ -34,34 +36,32 @@ if (reset.textContent === 'Start Game'){
 	clearOverlay('none', 'start');
 } else if (reset.textContent === 'Start a New Game') {
 	clearOverlay('none', 'win');
-
 } else if (reset.textContent === 'Try Again?') {
 	clearOverlay('none', 'lose');
 };
 
 function getRandomPhraseAsArray(arr){
     //do stuff to any arr that is passed in
-		const randomPhrases = Math.floor(Math.random() * arr.length);
+		var randomPhrases = Math.floor(Math.random() * arr.length);
 		return arr[randomPhrases].split("");
 }
 
 function addPhraseToDisplay(arr){
     // do stuff any arr that is passed in, and add to `#phrase ul`
-	  for(var i = 0; i < arr.length; i += 1) {
+	  for(var i = 0; i < arr.length; i++) {
 	    let li = document.createElement('li');
 			li.textContent = arr[i];
+			ul.appendChild(li);
 			if(arr[i] !== ' ') {
 				li.className = 'letter';
 			} else {
 				li.className = 'space';
 			}
-			ul.appendChild(li);
 	  }
-	  return arr;
+	  //return arr;
 }
 
 const phraseArray = getRandomPhraseAsArray(phrases);
-
 addPhraseToDisplay(phraseArray);
 
 function checkLetter(letterkeys) {
@@ -75,52 +75,55 @@ function checkLetter(letterkeys) {
 	return check;
 }
 
-function gamereset(){
 
-	if (resetGame === true) {
-		missed = 0;
-		//reset phrase letters
-		for (let i = 0; i < letters.length; i += 1) {
-		    ul.removeChild(letters[i]);
+function gamereset(){
+	missed = 0;
+
+	let li = document.querySelectorAll('.letter, .space');
+	for (var i = 0; i < li.length; i += 1) {
+    	ul.removeChild(li[i]);
+    }
+	//reset keyboard buttons
+ 	for (var i = 0; i < buttons.length; i++) {
+ 		buttons[i].className = "";
+ 		buttons[i].disabled = false;
+ 	}
+	//reset hearts
+	const olli = document.querySelector('.tries');
+	if (missed === 0) {
+ 		for (var i = 0; i < olli.length; i += 1) {
+			olli[i].style.display = "inline-block";
 		}
-		//reset keyboard buttons
-	 	for (var i = 0; i < buttons.length; i++) {
-	 		buttons[i].className = "";
-	 		buttons[i].disabled = false;
-	 	}
-		//reset hearts
-	 	for (var i = 0; i < 5; i += 1) {
-			let ol = scoreboard.getElementsByTagName('ol')[0];
-			let li = scoreboard.querySelector('li:last-child');
-			ol.addChild(li);
-	 	}
-		//reset phrase
-		const phraseArray = getRandomPhraseAsArray(phrases);
-		addPhraseToDisplay(phraseArray);
-	}
-	console.log(missed);
+ 	}
+
+	//reset phrase
+	const phraseArray = getRandomPhraseAsArray(phrases);
+	addPhraseToDisplay(phraseArray);
 }
 
 function checkwin(x, y, z){
-	let check = false;
 	if (x.length === y.length && z <= 5) {
 		reset.innerHTML = 'Start a New Game';
 		overlay.style.display = 'flex';
 		overlay.classList.add('win');
 		resetGame = true;
 	}
-	return check;
 }
 
 function checklose(){
-	let check = false;
+	missed+=1;
+	const ol = scoreboard.firstElementChild;
+	let li = ol.children[missed-1];
+	if (missed <= 5) {
+		li.style.display = "none";
+	}
+
 	if (missed > 5) {
 		reset.innerHTML = 'Try Again?';
 		overlay.style.display = 'flex';
 		overlay.classList.add('lose');
 		resetGame = true;
 	}
-	return check;
 }
 
 qwerty.addEventListener('click', (e) => {
@@ -128,21 +131,11 @@ qwerty.addEventListener('click', (e) => {
 	if (e.target.tagName === 'BUTTON'){
 	    e.target.classList.add('chosen');
 	    e.target.disabled = true;
-	 if(letterFound) {
-		checkwin(show, letters, missed);
-	} else if (!letterFound){
-		missed += 1;
-		let ol = scoreboard.getElementsByTagName('ol')[0];
-		let li = scoreboard.querySelector('li:last-child');
-		if (missed <= 5) {
-   			ol.removeChild(li);
-		} else {
+	 	if(letterFound) {
+			checkwin(show, letters, missed);
+	 	} else if (!letterFound){
 			checklose();
 		}
-	 }
   	}
 
 });
-
-
-gamereset();
